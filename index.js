dde1e49d81b2aa6ca69ae10d475ec5213654db22
JDC1492 <Jonathan_Cohen10@yahoo.com>
2020-11-19 23:07:03 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names, word){
+    let arr = []
+    for (let i = 0; i < names.length; i++) {
+        arr.push(`Thank you, ${names[i]}, for the wonderful ${word} gift!`)
+    }
+    return arr
+}
+
+function countDown(){
+    let i = 10
+    while (i >= 0) {
+        console.log (i--)
+    } 
+}
